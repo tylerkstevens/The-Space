33c081eb8a28dc06887fa243d0105c48a3466ec7
Tyler Stevens <tyler.stevens97@gmail.com>
2024-06-28 09:09:37 -0600

    google analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [partytown()],
   site: "https://denver.space",
   output: "server",
   adapter: node({
